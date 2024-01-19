25058dc7552304b73e0ca933d5216abb5b6a810e
Chidambararaja B <chidb@cisco.com>
2024-01-19 10:30:18 +0530

    Example files

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,11 @@
-import 'package:chidboilerplate/src/settings/settings_view.dart';
+import 'package:chidboilerplate/presentation/screens/example/example.dart';
+import 'package:chidboilerplate/presentation/screens/example/example_two.dart';
+import 'package:chidboilerplate/presentation/screens/settings/settings_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'settings/settings_controller.dart';
-import 'package:chidboilerplate/src/example/example.dart';
-import 'package:chidboilerplate/src/example/example_two.dart';
+import 'presentation/screens/settings/settings_controller.dart';
 import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
